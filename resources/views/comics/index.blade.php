8734c77c907f11f2830f7ae6f67d28b681d64af1
Giaki22 <giaki22@hotmail.com>
2022-07-21 10:05:52 +0200

    + D

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,6 +13,11 @@
                 <h2><a href="{{route('comics.show', $comic->id)}}">{{$comic->title}}</a></h2>
                 <p>{{$comic->description}}</p>
                 <h4>{{$comic->price}} $</h4>
+                <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">CANCELLA</button>
+                </form>
                 <hr>
             </li>
         @endforeach
